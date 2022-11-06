2279dc34c8e6e51453cf67e2a36e44483ff030a2
yoobinlee09 <114982668+yoobinlee09@users.noreply.github.com>
2022-11-07 02:29:17 +0900

    Add files via upload

---
+++ src/componets/views/LandingPage.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import MapContainer from './Sections/MapContainer'
+
+function LandingPage() {
+  const [InputText, setInputText] = useState('')
+  const [Place, setPlace] = useState('')
+
+  const onChange = (e) => {
+    setInputText(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setPlace(InputText)
+    setInputText('')
+  }
+
+  return (
+    <>
+      <form className="inputForm" onSubmit={handleSubmit}>
+        <input placeholder="검색어를 입력하세요" onChange={onChange} value={InputText} />
+        <button type="submit">검색</button>
+      </form>
+      <MapContainer searchPlace={Place} />
+    </>
+  )
+}
+
+export default LandingPage
